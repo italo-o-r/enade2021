9371471afe66ba7f28e1f32545717ae3d9af2112
italo-o-r <54160057+italo-o-r@users.noreply.github.com>
2021-05-03 21:38:33 -0300

    Update ProvaController.java

--- src/main/java/com/mycompany/enadeitalo2021/controller/ProvaController.java
+++ src/main/java/com/mycompany/enadeitalo2021/controller/ProvaController.java
@@ -8,6 +8,7 @@ package com.mycompany.enadeitalo2021.controller;
 
 import com.mycompany.enadeitalo2021.dao.ProvaDAO;
 import com.mycompany.enadeitalo2021.model.Prova;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.ManagedBean;
